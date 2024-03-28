4c2789712094e8fa84756680d7782177b71fb342
Karlos Aguirre <karlos.dev416@gmail.com>
2024-03-28 00:05:52 -0600

    Add custom data model

---
+++ MapApp/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  MapApp
+//
+//  Created by KARLOS AGUIRRE on 3/27/24.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
