12052518ecd1f1a2ef1f5637eeb72218ce45faa0
Karlos Aguirre <karlos.dev416@gmail.com>
2024-03-27 23:42:09 -0600

    Initial commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by KARLOS AGUIRRE on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
