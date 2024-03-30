beaf607ab8980638d7c467f05efdcfd9a8a5bdf1
Karlos Aguirre <karlos.dev416@gmail.com>
2024-03-30 11:02:14 -0600

    Use sheet to create a detail view for locations

---
+++ MapApp/Views/LocationDetailView.swift
@@ -0,0 +1,112 @@
+//
+//  LocationDetailView.swift
+//  MapApp
+//
+//  Created by KARLOS AGUIRRE on 3/30/24.
+//
+
+import SwiftUI
+import MapKit
+
+struct LocationDetailView: View {
+    
+    @EnvironmentObject private var vm: LocationsViewModel
+    
+    let location: Location
+    private let mapSpan = MKCoordinateSpan(latitudeDelta: 0.0095, longitudeDelta: 0.0095)
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                imageSection
+                    .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 20)
+                
+                VStack(alignment: .leading, spacing: 16.0) {
+                    titleSection
+                    Divider()
+                    descriptionSection
+                    Divider()
+                    mapLayer
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+            }
+        }
+        .ignoresSafeArea()
+        .background(.ultraThinMaterial)
+        .overlay(backButton, alignment: .topTrailing)
+    }
+}
+
+extension LocationDetailView {
+    private var imageSection: some View {
+        TabView {
+            ForEach(location.imageNames, id: \.self) {
+                Image($0)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: UIScreen.main.bounds.width)
+                    .clipped()
+            }
+        }
+        .frame(height: 500)
+        .tabViewStyle(PageTabViewStyle())
+    }
+    
+    private var titleSection: some View {
+        VStack(alignment: .leading, spacing: 8.0) {
+            Text(location.name)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            Text(location.cityName)
+                .font(.title3)
+                .foregroundColor(.secondary)
+        }
+    }
+    
+    private var descriptionSection: some View {
+        VStack(alignment: .leading, spacing: 8.0) {
+            Text(location.description)
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+            if let url = URL(string: location.link) {
+                Link("Read more on wikipedia", destination: url)
+                    .font(.headline)
+                    .tint(.blue)
+            }
+        }
+    }
+    
+    private var mapLayer: some View {
+        Map(coordinateRegion: .constant(MKCoordinateRegion(center: location.coordinates, span: mapSpan)),
+            annotationItems: [location]) { location in
+            MapAnnotation(coordinate: location.coordinates) {
+                LocationMapAnnotationView()
+                    .shadow(radius: 10)
+            }
+        }
+            .aspectRatio(1, contentMode: .fit)
+            .cornerRadius(30)
+            .allowsHitTesting(false)
+    }
+    
+    private var backButton: some View {
+        Button {
+            vm.sheetLocation = nil
+        } label: {
+            Image(systemName: "xmark")
+                .font(.headline)
+                .padding(16)
+                .foregroundColor(.primary)
+                .background(.thickMaterial)
+                .cornerRadius(10)
+                .shadow(radius: 4)
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    LocationDetailView(location: LocationsDataService.locations.first!)
+        .environmentObject(LocationsViewModel())
+}
--- MapApp/Views/LocationsView.swift
+++ MapApp/Views/LocationsView.swift
@@ -23,6 +23,9 @@ struct LocationsView: View {
                 locationsPreviewStack
             }
         }
+        .sheet(item: $vm.sheetLocation, onDismiss: nil) { location in
+            LocationDetailView(location: location)
+        }
     }
 }
 
